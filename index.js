67822bcdd080e4d00c5877498b2d652cbe434f8c
Irha Ali <irhaali_97@hotmail.ca>
2023-02-07 22:19:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const tmi = require('tmi.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: 'PiceaIrha',
+    password: 'ivwjmc96ljs8420sl33u1brux0jvhi'
+  },
+  channels: [
+    'mahum97'
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    return;
+  } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`); 
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice() {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
+
+
+const ttsTest = function() {
+  let message = new SpeechSynthesisUtterance();
+  message.text = 'Hello, world!';
+  message.lang = 'en-US';
+  window.speechSynthesis.speak(message);
+};
+
+const cancel = function() {
+  window.speechSynthesis.cancel();
+};
